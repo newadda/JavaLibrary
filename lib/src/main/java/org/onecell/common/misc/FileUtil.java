2027a0d486df4e17d0f64ab9e03d02eb58f56c08
Shin HyunHo <newadda@live.com>
2022-02-28 17:26:08 +0900

    소스 수정.

--- lib/src/main/java/org/onecell/common/misc/FileUtil.java
+++ lib/src/main/java/org/onecell/common/misc/FileUtil.java
@@ -8,6 +8,10 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.io.InputStream;
 import java.net.MalformedURLException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -23,6 +27,30 @@ public class FileUtil {
         }
         return true;
 
+    }
+
+    /// 이미지 여부 확인
+    public static boolean isImage(InputStream inputStream)
+    {
+        boolean result = false;
+        try {
+            BufferedImage read = ImageIO.read(inputStream);
+            if(read == null)
+            {
+                result=false;
+            }else
+            {
+                result =true;
+            }
+        } catch (IOException ioException) {
+            ioException.printStackTrace();
+        }
+        try {
+            inputStream.reset();
+        } catch (IOException ioException) {
+            ioException.printStackTrace();
+        }
+        return result;
     }
 
     public static String mimeType(String fileExtension)
@@ -51,4 +79,17 @@ public class FileUtil {
                 .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + resource.getFilename() + "\"")
                 .body(resource);
     }
+
+
+
+    public static ResponseEntity toResponseEntity(String file_path,String file_name,String mimeType) throws MalformedURLException {
+
+        Resource resource = new UrlResource(Paths.get(file_path).toFile().toURI());
+
+        return ResponseEntity.ok()
+                .contentType(MediaType.parseMediaType(mimeType))
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + file_name + "\"")
+                .body(resource);
+    }
+
 }
